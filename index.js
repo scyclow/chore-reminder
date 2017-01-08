6c9510e57a52e9dba86487e6d771ba36496b464a
scyclow <spike716@gmail.com>
2017-01-08 15:35:47 -0500

    fix heroku port stuff

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/', (req, res) => {
 });
 
 
-const port = 3003;
+const port = process.env.port || 3003;
 app.listen(port, (err) => {
   if (err) throw new Error(`Something went wrong with express: ${err}`);
 
